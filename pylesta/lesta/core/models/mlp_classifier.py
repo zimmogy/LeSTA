93726de01d40db105677aa273ef757012dd8d862
Palico <ftea.venus@gmail.com>
2026-04-01 21:42:54 +0800

    debug

--- pylesta/lesta/core/models/mlp_classifier.py
+++ pylesta/lesta/core/models/mlp_classifier.py
@@ -80,7 +80,8 @@ class MLPClassifier(nn.Module):
             mask = torch.ones_like(x)
             if torch.rand(1).item() < 0.5:
                 mask[:,5:7] = 0.0
-                x = x * mask
+            x = x * mask
+            
         if self.feature_normalizer is not None:
             x = self.feature_normalizer(x)
         
