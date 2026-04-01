7cd258fb93ab8b9f7b131eb52302151257c1a7ce
Palico <ftea.venus@gmail.com>
2026-04-02 00:04:52 +0800

    debug-3

--- lesta_ros/src/ros/label_generation_node.cpp
+++ lesta_ros/src/ros/label_generation_node.cpp
@@ -223,7 +223,7 @@ void LabelGenerationNode::publishLabelMap(const ros::TimerEvent &event) {
                                      lesta::layers::Feature::SLOPE,
                                      lesta::layers::Feature::ROUGHNESS,
                                      lesta::layers::Feature::CURVATURE,
-                                     lesta::layers::Feature::ELEVATION_VARIANCE,
+                                     lesta::layers::Feature::VARIANCE,
                                      lesta::layers::Label::FOOTPRINT,
                                      lesta::layers::Label::TRAVERSABILITY,
                                      // 新增的可视化层
